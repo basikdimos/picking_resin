b11058479e5f6d3accdce03cd0f0a1f4d5c649f2
Basik Dmitry <basik_dm@mail.ru>
2021-03-17 16:19:34 +0300

    Структура прототипа "Сбор живицы"

---
+++ addons/picking_resin/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Picking Resin",
+    'summary': """ Picking Resin managment """,
+    'description': """ Picking Resin business managent """,
+    'author': "STiM Dev Company",
+    'website':"None",
+    'category':'STiM',
+    'version':'14.0.0',
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'sequence': -100,
+}
